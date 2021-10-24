82ffccfef90b7741b11bfec0db2cbff3b784d9b3
Igor <Step>
2021-10-25 01:16:30 +0300

    Sliders with constrains

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSliderValue: UILabel!
+    @IBOutlet weak var redSliderValue: UILabel!
+    @IBOutlet weak var greenSliderValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        blueSlider.minimumValue = 0
+//        blueSlider.maximumValue = 1
+//        blueSlider.value = 0
+//        blueSlider.minimumTrackTintColor = .blue
+//        blueSlider.maximumTrackTintColor = .gray
+//        
+//        
+//        redSlider.minimumValue = 0
+//        redSlider.maximumValue = 1
+//        redSlider.value = 0
+//        redSlider.minimumTrackTintColor = .red
+//        redSlider.maximumTrackTintColor = .gray
+//        
+//        
+//        greenSlider.minimumValue = 0
+//        greenSlider.maximumValue = 1
+//        greenSlider.value = 0
+//        greenSlider.minimumTrackTintColor = .green
+//        greenSlider.maximumTrackTintColor = .gray
+        
     }
 
-
+    @IBAction func blueSliderAction() {
+    }
+    @IBAction func redSliderAction() {
+    }
+    
+    @IBAction func greedSliderAction() {
+    }
+    
+    
 }
 
